5d74b8e2c9f03c58fb11d2c0a20354844538f65f
lmachens <leon@machens.koeln>
2021-06-07 12:12:05 +0200

    🐛 Read samples relative to start script

--- src/api.ts
+++ src/api.ts
@@ -9,7 +9,7 @@ export type Talent = {
 
 export async function getTalent(id: string): Promise<Talent | undefined> {
   const talents: Talent[] = JSON.parse(
-    await fs.readFile(`${__dirname}/assets/samples/talents.json`, 'utf-8')
+    await fs.readFile('src/assets/samples/talents.json', 'utf-8')
   );
   const talent = talents.find((talent) => talent.id === id);
   return talent;
@@ -28,7 +28,7 @@ export type Course = {
 
 export async function getCourse(id: string): Promise<Course | undefined> {
   const courses: Course[] = JSON.parse(
-    await fs.readFile(`${__dirname}/assets/samples/courses.json`, 'utf-8')
+    await fs.readFile('src/assets/samples/courses.json', 'utf-8')
   );
   const course = courses.find((course) => course.id === id);
   return course;
