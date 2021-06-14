054a8caa1af73ca567cd4501c5cc68b52fbd4c84
phlgr <philipp@gartz.dev>
2021-06-14 14:26:19 +0200

    👽️ Fetch data from talent App API

--- src/api.ts
+++ src/api.ts
@@ -1,18 +1,81 @@
 import fs from 'fs/promises';
+import fetch from 'node-fetch';
 
 export type Talent = {
   id: string;
   firstName: string;
   lastName: string;
   courseId: string;
+  capstoneProject: CapstoneProject;
 };
 
+export type CapstoneProject = {
+  isDesktop: boolean;
+  title: string;
+  subtitle: string;
+  description: string;
+  technologies: string[];
+  thumbnail: string;
+};
+
+export type TalentFromTalentApp = {
+  avatar: {
+    uploaded: number;
+    urls: { lg: string; s: string; xl: string; m: string };
+    id: string;
+    sizes: { value: number; key: string }[];
+    uploaded_by: string;
+  };
+  project: {
+    projectDesc: string;
+    techStack: string[];
+    projectName: string;
+    projectRepoUrl: string;
+    projectImage: {
+      uploaded: number;
+      urls: { lg: string; s: string; xl: string; m: string };
+      id: string;
+      sizes: { value: number; key: string }[];
+      uploadedBy: string;
+    };
+    projectAppUrl: string;
+  };
+  termsOfUseAccepted: boolean;
+  email: string;
+  firstName: string | null;
+  lastName: string | null;
+  gitHub: string;
+  online: boolean;
+  livingLocation: string | null;
+  employed: boolean;
+  alumniLocation: string;
+  lookingFor: string[];
+  roles: string[];
+  region: string;
+  skills: string[];
+  id: string;
+};
+
+const API_URL = process.env.API_URL;
+
 export async function getTalent(id: string): Promise<Talent | undefined> {
-  const talents: Talent[] = JSON.parse(
-    await fs.readFile('src/assets/samples/talents.json', 'utf-8')
-  );
-  const talent = talents.find((talent) => talent.id === id);
-  return talent;
+  const response = await fetch(`${API_URL}/${id}`);
+  const talent: TalentFromTalentApp = await await response.json();
+  const formattedTalent: Talent = {
+    firstName: talent.firstName,
+    lastName: talent.lastName,
+    id: talent.id,
+    courseId: 'web-cgn-21-2',
+    capstoneProject: {
+      title: talent.project.projectName,
+      subtitle: '',
+      description: talent.project.projectDesc,
+      isDesktop: false,
+      technologies: talent.project.techStack,
+      thumbnail: talent.project.projectImage.urls.xl,
+    },
+  };
+  return formattedTalent;
 }
 
 export type CourseTopics = {
