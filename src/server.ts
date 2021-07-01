ace988ddc6ddea2e25b7e90895a0c57df9254a8b
phlgr <philipp@gartz.dev>
2021-07-01 15:26:35 +0200

    🚑 Filter Diacritis in download name

--- src/server.ts
+++ src/server.ts
@@ -52,7 +52,11 @@ http
         }
         res.setHeader(
           'Content-Disposition',
-          `inline; filename=${talent.firstName}_${talent.lastName}_certificate.pdf`
+          `inline; filename=${talent.firstName
+            .normalize('NFD')
+            .replace(/\p{Diacritic}/gu, '')}_${talent.lastName
+            .normalize('NFD')
+            .replace(/\p{Diacritic}/gu, '')}_certificate.pdf`
         );
 
         responseCertificate(res, talent, course);
